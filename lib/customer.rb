47962a31e94a684cff4638fa06bfc1edcbcba616
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-26 01:03:25 -0500

    commit tree dcd73baa7c10ca49d3b7c55e08ec169480f8fa4a

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
-  
+  attr_accessor :name, :age
+  @@all = []
+
+  def initialize(name, age)
+    self.name = name
+    self.age = age
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip = 0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select{ |meal| meal.customer == self }
+  end
+
+  def waiters
+    meals.collect { |meal| meal.waiter }
+  end
 end
